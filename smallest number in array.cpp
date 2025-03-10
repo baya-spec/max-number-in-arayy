b27ccdcb56442ba932fe4670d380c72c030a5953
baya-spec <bayagb822@gmail.com>
2025-03-10 23:45:57 +0300

    smallest nimber in array a c++ code

---
+++ smallest number in array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int smallest = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < smallest) {
+            smallest = arr[i];
+        }
+    }
+
+    cout << "The smallest number in the array is: " << smallest << endl;
+
+    return 0;
+}
